c0bfd3a678645464de9137eef1e6ad1cbc80a241
olsona39 <olsona39@wwu.edu>
2019-05-08 18:54:16 -0700

    adding courses

--- DB/RegistrationSystem.java
+++ DB/RegistrationSystem.java
@@ -2,7 +2,6 @@ import java.sql.*;
 import java.lang.*;
 import java.util.*;
 
-
 public class RegistrationSystem{
 
     public static String ConnectionString;
@@ -33,46 +32,24 @@ public class RegistrationSystem{
         //Prompt user for command
         String operation = "";
         while(!operation.equals("Exit")){
-            System.out.println("Select an operation: Get Transcript, Check Degree, Requirements, Add Course, Remove Course, or Exit");
+            System.out.println("Select an operation: Get Transcript, Check Degree Requirements, Add Course, Remove Course, or Exit");
             operation = scanner.nextLine();
             //Use sID to query db to get xcourse number, xcourse title, xsemester, xyear, xgrade, credits of every course student has taken in chronological order.
             if(operation.equals("Get Transcript")){
                 System.out.println("Getting Transcript");
-                try {
-                    String query = "SELECT C.course_id, C.title, T.semester, T.year, T.grade, C.credits FROM course AS C, student AS S, takes AS T WHERE S.id = T.id AND T.course_id = C.course_id AND S.id = " + sID + ";";
-                    stmt = connection.createStatement();
-                    ResultSet rs = stmt.executeQuery(query);
-                    while (rs.next()) {
-                        String courseNum = rs.getString("C.course_id");
-                        String courseTitle = rs.getString("C.title");
-                        String semester = rs.getString("T.semester");
-                        String year = rs.getString("T.year");
-                        String grade = rs.getString("T.grade");
-                        String credits = rs.getString("C.credits");
-
-                        System.out.println("Course Number: " + courseNum + " Course Title: " + courseTitle + " Semester: " + semester + " Year: " + year + " Grade: " + grade + " credits: " + credits );
-
-                    }
-                }catch(SQLException e){
-                    e.printStackTrace();
-                }
+                getTranscript(connection, sID);
             }
-//            SELECT DISTINCT C.course_id
-//                    -- C.course_id, T.course_id, S.ID, C.dept_name, C.title
-//            FROM takes AS T, course AS C, student AS S
-//            WHERE S.ID = T.ID
-//            AND C.dept_name = S.dept_name
-//            AND T.course_id != C.course_id
-//            AND S.ID = '12345';
-            if(operation == "Check Degree Requirements") {
-
+            if(operation.equals("Check Degree Requirements")) {
+                System.out.println("checking degree requirements");
+                getDegreeRequirements(connection, sID);
             }
-            if(operation == "Add Course"){
-
+            if(operation.equals("Add Course")){
+                System.out.println("adding course.");
+                addCourse(connection, sID);
             }
-            if(operation == "Remove Course"){
-
-            }
+//            if(operation == "Remove Course"){
+//
+//            }
 
 
 
@@ -95,6 +72,80 @@ public class RegistrationSystem{
 
     }
 
+    public static void getTranscript(Connection connection, String sID){
+        try {
+            String query = "SELECT C.course_id, C.title, T.semester, T.year, T.grade, C.credits FROM course AS C, student AS S, takes AS T WHERE S.id = T.id AND T.course_id = C.course_id AND S.id = " + sID + ";";
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            while (rs.next()) {
+                String courseNum = rs.getString("C.course_id");
+                String courseTitle = rs.getString("C.title");
+                String semester = rs.getString("T.semester");
+                String year = rs.getString("T.year");
+                String grade = rs.getString("T.grade");
+                String credits = rs.getString("C.credits");
+
+                System.out.println("\nCourse Number: " + courseNum + "\nCourse Title: " + courseTitle + "\nSemester: " + semester + "\nYear: " + year + "\nGrade: " + grade + "\ncredits: " + credits );
+                System.out.println();
+            }
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void getDegreeRequirements(Connection connection, String sID) {
+        try {
+            String query =
+                    "        SELECT C.course_id, C.title\n" +
+                            "        FROM student AS S, course AS C\n" +
+                            "        WHERE S.ID = " + sID + "\n" +
+                            "        AND S.dept_name = C.dept_name\n" +
+                            "        AND C.course_id NOT IN\n" +
+                            "                (SELECT C.course_id\n" +
+                            "                        FROM takes AS T, course AS C, student AS S\n" +
+                            "                        WHERE T.ID = S.ID\n" +
+                            "                        AND C.course_id = T.course_id\n" +
+                            "                        AND C.dept_name = S.dept_name\n" +
+                            "                        AND S.ID = " + sID;
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            while (rs.next()) {
+                String courseNum = rs.getString("C.course_id");
+                String courseTitle = rs.getString("C.title");
+
+                System.out.println("\nCourse Number: " + courseNum + " \nCourse Title: " + courseTitle);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void addCourse(Connection connection, String sID){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Which Semester?");
+        String semeseter = scanner.nextLine();
+        System.out.println("Which Year?");
+        String year = scanner.nextLine();
+        int count = 1;
+        try{
+            String query =
+                    "SELECT course_id, sec_id\n" +
+                            "FROM section\n" +
+                            "WHERE year = '2009'\n" +
+                            "AND semester = 'Spring'";
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            while(rs.next()){
+                String courseNum = rs.getString("course_id");
+                String sectionId = rs.getString("sec_id");
+                System.out.println("\n" + count + ":\n Course Number: " + courseNum + "\nSection id:" + sectionId);
+                count++;
+            }
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+
+    }
     public static Connection getConnection(){
         Connection conn = null;
         try {
